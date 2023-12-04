b20e0624b9cc89b41db81ed0f29f01b9210f4287
tigertrack <riandyfadly@gmail.com>
2023-12-04 22:32:48 +0900

    feat: adding input form and data table within the same repository

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-
+const path = require('path');
 const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(cors());
 app.use(express.json());
@@ -11,6 +13,10 @@ app.use(express.urlencoded({ extended: true }));
 const bookRoutes = require('./routes/bookRoutes');
 
 app.use(bookRoutes);
+
+app.get('/foo', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'booklist.html'));
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
